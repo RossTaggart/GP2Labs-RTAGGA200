4748dccdd2f90ff0a8f1f584045c9b9ce9964961
RossTaggart <rosstaggart12@gmail.com>
2014-09-23 10:16:19 +0100

    Creation of Window

---
+++ GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/main.cpp
@@ -0,0 +1,45 @@
+//Header files
+#include <iostream>
+//header for SDL2 functionality
+#include<SDL.h>
+
+//Global variables go here
+//Pointer to our SDL Windows
+SDL_Window*window;
+
+//Constraints to control window creation
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+
+//Global functions
+void InitWindow(int width, int height, bool fullscreen){
+
+	//Create a window
+	window = SDL_CreateWindow("Lab1",//window title
+								SDL_WINDOWPOS_CENTERED, //x position, centered
+								SDL_WINDOWPOS_CENTERED, //y position, centered
+								width, //width, in pixels
+								height, //height, in pixels
+								SDL_WINDOW_OPENGL //flags
+								);
+}
+
+//Used to cleanup once we exit
+void CleanUp(){
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+//Main Method - Entry Point
+int main(int argc, char* arg[]){
+	//init everything - SDL, if it is nonzero we have a problem
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout << "ERROR SDL_Init" << SDL_GetError() << std::endl;
+
+		return-1;
+	}
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false);
+
+	CleanUp();
+}
