9cbe72a5484e7945a5eda5af5a77c772ad904ba0
RossTaggart <rosstaggart12@gmail.com>
2014-09-23 10:42:37 +0100

    Adding OpenGL

--- GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/main.cpp
+++ GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/main.cpp
@@ -2,10 +2,14 @@
 #include <iostream>
 //header for SDL2 functionality
 #include<SDL.h>
+#include<SDL_opengl.h>
+#include<gl\GLU.h>
 
 //Global variables go here
 //Pointer to our SDL Windows
 SDL_Window*window;
+SDL_GLContext glcontext;
+
 bool running = true;
 
 //Constraints to control window creation
@@ -27,8 +31,72 @@ void InitWindow(int width, int height, bool fullscreen){
 
 //Used to cleanup once we exit
 void CleanUp(){
+	SDL_GL_DeleteContext(glcontext);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+}
+
+//Function to initialise OpenGL
+void initOpenGL(){
+
+	//Create OpenGL Context
+	glcontext = SDL_GL_CreateContext(window);
+	//Something went wrong in creating the context, if it is still NULL
+	if (!glcontext){
+
+		std::cout << "Error Creating OpenGL Context" << SDL_GetError() <<
+		std::endl;
+	}
+
+	//Smooth shading
+	glShadeModel(GL_SMOOTH);
+
+	//clear the background to black
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	//Clear the depth buffer to 1.0
+	glClearDepth(1.0f);
+
+	//Enable depth testing
+	glEnable(GL_DEPTH_TEST);
+
+	//The depth test to use
+	glDepthFunc(GL_LEQUAL);
+
+	//Turn on best perspective correction
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+}
+
+//Function to set/reset viewport
+void setViewport(int width, int height){
+
+	//screen ration
+	GLfloat ratio;
+
+	//make sure height is always above 0
+	if (height == 0){
+		height = 1;
+	}
+
+	//calculate creen ration
+	ratio = (GLfloat)width / (GLfloat)height;
+
+	//Setup viewport
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	//Change to project matrix mode
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	//Calculate perspective matrix, using glu library functions
+	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+
+	//Switch to ModelView
+	glMatrixMode(GL_MODELVIEW);
+
+	//Reset using the Identity Matrix
+	glLoadIdentity();
 }
 
 //Main Method - Entry Point
@@ -41,6 +109,11 @@ int main(int argc, char* arg[]){
 		return-1;
 	}
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false);
+
+	//Call our InitOpenGL Function
+	initOpenGL();
+	//set our viewport
+	setViewport(WINDOW_WIDTH, WINDOW_HEIGHT);
 
 	SDL_Event event;
 	while (running)
