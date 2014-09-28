35eeffeaa3ab653a757d50aa0320fb3642e796fc
RossTaggart <rosstaggart12@gmail.com>
2014-09-28 23:07:35 +0100

    Changed triangle to be right angled

--- GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/main.cpp
+++ GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/main.cpp
@@ -116,8 +116,10 @@ void render(){
 	glBegin(GL_TRIANGLES);
 		glColor3f(1.0f, 0.0f, 0.0f); //Color of vertices
 		glVertex3f(0.0f, 1.0f, 0.0f); //Top
-		glVertex3f(-1.0f, -1.0f, 0.0f); //Bottom Left
+		glVertex3f(0.0f, -1.0f, 0.0f); //Bottom Left
 		glVertex3f(1.0f, -1.0f, 0.0f); //Bottom Right
+
+		//glVertex3f()
 	glEnd();
 
 
