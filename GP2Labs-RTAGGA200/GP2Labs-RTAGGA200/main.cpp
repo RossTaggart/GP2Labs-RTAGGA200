7dbea22c9513151956ce5ad2958ae25976bda943
RossTaggart <rosstaggart12@gmail.com>
2014-10-28 09:42:02 +0000

    more code refactoring - COMPLETE

--- GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/main.cpp
+++ GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/main.cpp
@@ -3,42 +3,52 @@
 #include "Vertex.h"
 #include "Shader.h"
 #include "Texture.h"
+
 //header for SDL2 functionality
 #include <gl\glew.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <gl\GLU.h>
 #include <SDL_ttf.h>
+
 //maths headers
 #include <glm/glm.hpp>
 using glm::mat4;
 using glm::vec3;
+
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
 #ifdef _DEBUG && WIN32
-const std::string ASSET_PATH = "assets";
+const std::string ASSET_PATH = "../assets";
 #else
 const std::string ASSET_PATH = "assets";
 #endif
 const std::string SHADER_PATH = "/shaders";
 const std::string TEXTURE_PATH = "/textures";
 const std::string FONT_PATH = "/fonts";
+
 //Global variables go here
 //Pointer to our SDL Windows
 SDL_Window * window;
 //SDL GL Context
 SDL_GLContext glcontext = NULL;
+
 //Shader Programs
 GLuint shaderProgram2d = 0;
 GLuint shaderProgram3d = 0;
+
 //matrices
 mat4 viewMatrix;
+
 //2D matrices
 mat4 projMatrix2d;
 mat4 worldMatrix2d;
+
 //3D matrices
 mat4 projMatrix3d;
 mat4 worldMatrix3d;
+
 //Triangle 1 Vetex positions
 //Triangle 1 Vertex 1
 float triangle1vertex1PosX = 0.1f;
@@ -65,54 +75,56 @@ float triangle2vertex2PosZ = 0.0f;
 float triangle2vertex3PosX = -1.1f;
 float triangle2vertex3PosY = -1.0f;
 float triangle2vertex3PosZ = 0.0f;
+
 //Constants to control window creation
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
+
 bool running = true;
-/*float triangleData[] = { 0.0f, 1.0f, 0.0f, // Top
--1.0f, -1.0f, 0.0f, // Bottom Left
-1.0f, -1.0f, 0.0f }; //Bottom Right
-*/
-/*Vertex triangleData[] = { { 0.0f, 1.0f, 0.0f, //x,y,z
-1.0f, 0.0f, 0.0f, 1.0f }, //r,g,b,a
-{ -1.0f, -1.0f, 0.0f, //x,y,z
-0.0f, 1.0f, 0.0f, 1.0f }, //r,g,b,a
-{ 1.0f, -1.0f, 0.0f, //x,y,z
-0.0f, 0.0f, 1.0f, 1.0f } }; //r,g,b,a
-*/
-Vertex triangleData[] = {
+
+
+Vertex triangleData[] = 
+{
 	//Front
 		{ vec3(-0.5f, 0.5f, 0.5f), vec2(0.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Left
 		{ vec3(-0.5f, -0.5f, 0.5f), vec2(0.0f, 1.0f), vec4(1.0f, 1.0f, 0.0f, 1.0f) }, //Bottom Left
 		{ vec3(0.5f, -0.5f, 0.5f), vec2(1.0f, 1.0f), vec4(0.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Right
 		{ vec3(0.5f, 0.5f, 0.5f), vec2(1.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Right
+
 		//Back
 		{ vec3(-0.5f, 0.5f, -0.5f), vec2(0.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Left
 		{ vec3(-0.5f, -0.5f, -0.5f), vec2(0.0f, 1.0f), vec4(1.0f, 1.0f, 0.0f, 1.0f) }, //Bottom Left
 		{ vec3(0.5f, -0.5f, -0.5f), vec2(1.0f, 1.0f), vec4(0.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Right
 		{ vec3(0.5f, 0.5f, -0.5f), vec2(1.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Right
+
 		//Left
 		{ vec3(-0.5f, 0.5f, -0.5f), vec2(0.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Left
 		{ vec3(-0.5f, -0.5f, -0.5f), vec2(0.0f, 1.0f), vec4(1.0f, 1.0f, 0.0f, 1.0f) }, //Bottom Left
 		{ vec3(-0.5f, -0.5f, 0.5f), vec2(1.0f, 1.0f), vec4(0.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Right
 		{ vec3(-0.5f, 0.5f, 0.5f), vec2(1.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Right
+
 		//Right
 		{ vec3(0.5f, 0.5f, 0.5f), vec2(0.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Left
 		{ vec3(0.5f, -0.5f, 0.5f), vec2(0.0f, 1.0f), vec4(1.0f, 1.0f, 0.0f, 1.0f) }, //Bottom Left
 		{ vec3(0.5f, -0.5f, -0.5f), vec2(1.0f, 1.0f), vec4(0.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Right
 		{ vec3(0.5f, 0.5f, -0.5f), vec2(1.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Right
+
 		//Top
 		{ vec3(-0.5f, 0.5f, -0.5f), vec2(0.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Left
 		{ vec3(-0.5f, 0.5f, 0.5f), vec2(0.0f, 1.0f), vec4(1.0f, 1.0f, 0.0f, 1.0f) }, //Bottom Left
 		{ vec3(0.5f, 0.5f, 0.5f), vec2(1.0f, 1.0f), vec4(0.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Right
 		{ vec3(0.5f, 0.5f, -0.5f), vec2(1.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Right
+
 		//Bottom
 		{ vec3(-0.5f, -0.5f, 0.5f), vec2(0.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Left
 		{ vec3(-0.5f, -0.5f, -0.5f), vec2(0.0f, 1.0f), vec4(1.0f, 1.0f, 0.0f, 1.0f) }, //Bottom Left
 		{ vec3(0.5f, -0.5f, -0.5f), vec2(1.0f, 1.0f), vec4(0.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Right
 		{ vec3(0.5f, -0.5f, 0.5f), vec2(1.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Right
+
 };
-GLuint indices[] = {
+
+GLuint indices[] = 
+{
 	//Front
 	0, 1, 2,
 	0, 3, 2,
@@ -132,24 +144,21 @@ GLuint indices[] = {
 	4, 5, 6,
 	4, 7, 6
 };
-/*
-GLuint triangleVBO;
-GLuint triangleEBO;
-GLuint VAO;
-GLuint texture = 0;
-GLuint fontTexture = 0;
-*/
+
 //2D
 GLuint VBO2d;
 GLuint EBO2d;
 GLuint VAO2d;
+
 //3D
 GLuint VBO3d;
 GLuint EBO3d;
 GLuint VAO3d;
+
 //Textures
 GLuint texture = 0;
 GLuint fontTexture = 0;
+
 //Global functions
 void InitWindow(int width, int height, bool fullscreen)
 {
@@ -162,6 +171,7 @@ void InitWindow(int width, int height, bool fullscreen)
 		SDL_WINDOW_OPENGL	//flags
 		);
 }
+
 void CleanUp2D()
 {
 	glDeleteTextures(1, &fontTexture);
@@ -170,6 +180,7 @@ void CleanUp2D()
 	glDeleteBuffers(1, &VBO2d);
 	glDeleteVertexArrays(1, &VAO2d);
 }
+
 void CleanUp3D()
 {
 	glDeleteTextures(1, &texture);
@@ -179,6 +190,7 @@ void CleanUp3D()
 	glDeleteBuffers(1, &VBO3d);
 	glDeleteVertexArrays(1, &VAO3d);
 }
+
 //Used to cleanup once we exit
 void CleanUp()
 {
@@ -188,6 +200,7 @@ void CleanUp()
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
+
 //Function to initialise OpenGL
 void initOpenGL()
 {
@@ -201,6 +214,7 @@ void initOpenGL()
 	{
 		std::cout << "Error Creating OpenGL Context" << SDL_GetError() << std::endl;
 	}
+
 	//Smooth shading
 	glShadeModel(GL_SMOOTH);
 	//clear the background to black
@@ -214,6 +228,7 @@ void initOpenGL()
 	//Turn on best perspective correction
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 	glewExperimental = GL_TRUE;
+
 	GLenum err = glewInit();
 	if (GLEW_OK != err)
 	{
@@ -221,6 +236,7 @@ void initOpenGL()
 		std::cout << "Error: " << glewGetErrorString(err) << std::endl;
 	}
 }
+
 //Function to set/reset viewport
 void setViewport(int width, int height)
 {
@@ -236,205 +252,179 @@ void setViewport(int width, int height)
 	//Setup viewport
 	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
 }
+
 void render2D()
 {
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	glBindVertexArray(VAO2d);
 	glUseProgram(shaderProgram2d);
+
 	GLint MVPLocation = glGetUniformLocation(shaderProgram2d, "MVP");
 	mat4 MVP = projMatrix2d*viewMatrix*worldMatrix2d;
 	glUniformMatrix4fv(MVPLocation, 1, GL_FALSE, glm::value_ptr(MVP));
+	
 	GLuint texture0Location = glGetUniformLocation(shaderProgram2d, "texture0");
 	glActiveTexture(GL_TEXTURE);
 	glBindTexture(GL_TEXTURE_2D, fontTexture);
 	glUniform1i(texture0Location, 0);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
+
 void render3D()
 {
 	glBindVertexArray(VAO3d);
 	glUseProgram(shaderProgram3d);
+
 	GLint MVPLocation = glGetUniformLocation(shaderProgram3d, "MVP");
 	mat4 MVP = projMatrix3d*viewMatrix*worldMatrix3d;
 	glUniformMatrix4fv(MVPLocation, 1, GL_FALSE, glm::value_ptr(MVP));
+
 	GLuint texture0Location = glGetUniformLocation(shaderProgram3d, "texture0");
 	glActiveTexture(GL_TEXTURE);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glUniform1i(texture0Location, 0);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
+
 //Function to draw
 void render()
 {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	/*
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glBindBuffer(GL_ARRAY_BUFFER, triangleVBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangleEBO);
-	glBindVertexArray(VAO);
-	glUseProgram(shaderProgram);
-	GLint MVPLocation = glGetUniformLocation(shaderProgram, "MVP");
-	mat4 MVP = projMatrix*viewMatrix*worldMatrix;
-	glUniformMatrix4fv(MVPLocation, 1, GL_FALSE, glm::value_ptr(MVP));
-	GLuint texture0Location = glGetUniformLocation(shaderProgram, "texture0");
-	glActiveTexture(GL_TEXTURE);
-	glBindTexture(GL_TEXTURE_2D, fontTexture);
-	glUniform1i(texture0Location, 0);
-	//Tell the shader that 0 is the position element
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)sizeof(vec3));
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)(sizeof(vec3) + sizeof(vec2)));
-	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-	*/
+
 	render2D();
 	render3D();
+
 	SDL_GL_SwapWindow(window);
 }
-/*void initGeometry()
-{
-glGenVertexArrays(1, &VAO);
-glBindVertexArray(VAO);
-//Create buffer
-glGenBuffers(1, &triangleVBO);
-//Make the new VBO active
-glBindBuffer(GL_ARRAY_BUFFER, triangleVBO);
-//Copy Vertex Data to VBO
-glBufferData(GL_ARRAY_BUFFER, sizeof(triangleData), triangleData, GL_STATIC_DRAW);
-//Create buffer
-glGenBuffers(1, &triangleEBO);
-//Make the new EBO active
-glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangleEBO);
-//Copy Index Data to the EBO
-glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-}*/
-/*void initGeometryFromTexture(GLuint textureID)
-{
-int width, height;
-glBindTexture(GL_TEXTURE_2D, textureID);
-glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &width);
-glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &height);
-Vertex spriteData[] = {
-{ vec3(0.0f, 0.0f, 0.0f), vec2(0.0f, 0.0f), vec4(1.0f, 1.0f, 1.0f, 1.0f) }, //Top Left
-{ vec3(0.0f, height, 0.0f), vec2(0.0f, 1.0f), vec4(1.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Left
-{ vec3(width, height, 0.0f), vec2(1.0f, 1.0f), vec4(1.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Right
-{ vec3(width, 0.0f, 0.0f), vec2(1.0f, 0.0f), vec4(1.0f, 1.0f, 1.0f, 1.0f) } //Top Right
-};
-GLuint spriteIndices[] = {
-0, 1, 2,
-0, 3, 2,
-};
-glGenVertexArrays(1, &VAO);
-glBindVertexArray(VAO);
-glGenBuffers(1, &triangleVBO);
-glBindBuffer(GL_ARRAY_BUFFER, triangleVBO);
-glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(Vertex), spriteData, GL_STATIC_DRAW);
-glGenBuffers(1, &triangleEBO);
-glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triangleEBO);
-glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(int), spriteIndices, GL_STATIC_DRAW);
-glEnableVertexAttribArray(0);
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL);
-glEnableVertexAttribArray(1);
-glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)sizeof(vec3));
-glEnableVertexAttribArray(2);
-glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)(sizeof(vec3) + sizeof(vec2)));
-}*/
+
 void create2DScene()
 {
 	std::string fontPath = ASSET_PATH + FONT_PATH + "/OratorStd.otf";
 	fontTexture = loadTextureFromFont(fontPath, 64, "Hello!");
+
 	GLuint vertexShaderProgram = 0;
+
 	std::string vsPath = ASSET_PATH + SHADER_PATH + "/textureVS.glsl";
 	vertexShaderProgram = loadShaderFromFile(vsPath, VERTEX_SHADER);
+
 	GLuint fragmentShaderProgram = 0;
+
 	std::string fsPath = ASSET_PATH + SHADER_PATH + "/textureFS.glsl";
 	fragmentShaderProgram = loadShaderFromFile(fsPath, FRAGMENT_SHADER);
+
 	shaderProgram2d = glCreateProgram();
+
 	glAttachShader(shaderProgram2d, vertexShaderProgram);
 	glAttachShader(shaderProgram2d, fragmentShaderProgram);
 	glLinkProgram(shaderProgram2d);
 	checkForLinkErrors(shaderProgram2d);
+
 	//now we can delete the VS & FS Programs
 	glDeleteShader(vertexShaderProgram);
 	glDeleteShader(fragmentShaderProgram);
+
 	glBindAttribLocation(shaderProgram2d, 0, "vertexPosition");
 	glBindAttribLocation(shaderProgram2d, 1, "vertexTexCoords");
 	glBindAttribLocation(shaderProgram2d, 2, "vertexColour");
+
 	int width, height;
+
 	glBindTexture(GL_TEXTURE_2D, fontTexture);
 	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &width);
 	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &height);
-	Vertex spriteData[] = {
+	Vertex spriteData[] = 
+	{
 			{ vec3(0.0f, 0.0f, 0.0f), vec2(0.0f, 0.0f), vec4(1.0f, 1.0f, 1.0f, 1.0f) }, //Top Left
 			{ vec3(0.0f, height, 0.0f), vec2(0.0f, 1.0f), vec4(1.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Left
 			{ vec3(width, height, 0.0f), vec2(1.0f, 1.0f), vec4(1.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Right
 			{ vec3(width, 0.0f, 0.0f), vec2(1.0f, 0.0f), vec4(1.0f, 1.0f, 1.0f, 1.0f) } //Top Right
 	};
-	GLuint spriteIndices[] = {
+
+	GLuint spriteIndices[] = 
+	{
 		0, 1, 2,
 		0, 3, 2,
 	};
+
 	glGenVertexArrays(1, &VAO2d);
 	glBindVertexArray(VAO2d);
+
 	glGenBuffers(1, &VBO2d);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO2d);
+
 	glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(Vertex), spriteData, GL_STATIC_DRAW);
+
 	glGenBuffers(1, &EBO2d);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2d);
+
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(int), spriteIndices, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL);
+
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)sizeof(vec3));
+
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)(sizeof(vec3) + sizeof(vec2)));
 }
+
 void create3DScene()
 {
 	std::string texturePath = ASSET_PATH + TEXTURE_PATH + "/texture.png";
 	texture = loadTextureFromFile(texturePath);
+
 	GLuint vertexShaderProgram = 0;
 	std::string vsPath = ASSET_PATH + SHADER_PATH + "/textureVS.glsl";
 	vertexShaderProgram = loadShaderFromFile(vsPath, VERTEX_SHADER);
+
 	GLuint fragmentShaderProgram = 0;
 	std::string fsPath = ASSET_PATH + SHADER_PATH + "/textureFS.glsl";
+
 	fragmentShaderProgram = loadShaderFromFile(fsPath, FRAGMENT_SHADER);
+
 	shaderProgram3d = glCreateProgram();
 	glAttachShader(shaderProgram3d, vertexShaderProgram);
 	glAttachShader(shaderProgram3d, fragmentShaderProgram);
 	glLinkProgram(shaderProgram3d);
 	checkForLinkErrors(shaderProgram3d);
+
 	//now we can delete the VS & FS Programs
 	glDeleteShader(vertexShaderProgram);
 	glDeleteShader(fragmentShaderProgram);
 	glBindAttribLocation(shaderProgram3d, 0, "vertexPosition");
 	glBindAttribLocation(shaderProgram3d, 1, "vertexTexCoords");
 	glBindAttribLocation(shaderProgram3d, 2, "vertexColour");
-	Vertex triangleData[] = {
+	Vertex triangleData[] = 
+	{
 		//Front
 			{ vec3(-0.5f, 0.5f, 0.5f), vec2(0.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Left
 			{ vec3(-0.5f, -0.5f, 0.5f), vec2(0.0f, 1.0f), vec4(1.0f, 1.0f, 0.0f, 1.0f) }, //Bottom Left
 			{ vec3(0.5f, -0.5f, 0.5f), vec2(1.0f, 1.0f), vec4(0.0f, 1.0f, 1.0f, 1.0f) }, //Bottom Right
 			{ vec3(0.5f, 0.5f, 0.5f), vec2(1.0f, 0.0f), vec4(1.0f, 0.0f, 1.0f, 1.0f) }, //Top Right
 	};
-	GLuint indices[] = {
+
+	GLuint indices[] = 
+	{
 		//Front
 		0, 1, 2,
 		0, 3, 2,
 	};
+
 	glGenVertexArrays(1, &VAO3d);
 	glBindVertexArray(VAO3d);
+
 	glGenBuffers(1, &VBO3d);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO3d);
+
 	glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(Vertex), triangleData, GL_STATIC_DRAW);
 	glGenBuffers(1, &EBO3d);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO3d);
+
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(int), indices, GL_STATIC_DRAW);
+
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL);
 	glEnableVertexAttribArray(1);
@@ -442,55 +432,28 @@ void create3DScene()
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)(sizeof(vec3) + sizeof(vec2)));
 }
-/*void createShader()
-{
-GLuint vertexShaderProgram = 0;
-std::string vsPath = ASSET_PATH + SHADER_PATH + "/textureVS.glsl";
-vertexShaderProgram = loadShaderFromFile(vsPath, VERTEX_SHADER);
-GLuint fragmentShaderProgram = 0;
-std::string fsPath = ASSET_PATH + SHADER_PATH + "/textureFS.glsl";
-fragmentShaderProgram = loadShaderFromFile(fsPath, FRAGMENT_SHADER);
-shaderProgram = glCreateProgram();
-glAttachShader(shaderProgram, vertexShaderProgram);
-glAttachShader(shaderProgram, fragmentShaderProgram);
-glLinkProgram(shaderProgram);
-checkForLinkErrors(shaderProgram);
-//now we can delete the VS & FS Programs
-glDeleteShader(vertexShaderProgram);
-glDeleteShader(fragmentShaderProgram);
-glBindAttribLocation(shaderProgram, 0, "vertexPosition");
-glBindAttribLocation(shaderProgram, 1, "vertexTexCoords");
-glBindAttribLocation(shaderProgram, 2, "vertexColour");
-}*/
-/*void createTexture()
-{
-std::string texturePath = ASSET_PATH + TEXTURE_PATH + "/texture.png";
-texture = loadTextureFromFile(texturePath);
-}*/
-/*void createFontTexture()
-{
-std::string fontPath = ASSET_PATH + FONT_PATH + "/OratorStd.otf";
-fontTexture = loadTextureFromFont(fontPath, 64, "Hello!");
-}*/
+
 void Update2D()
 {
 	viewMatrix = glm::lookAt(vec3(0.0f, 0.0f, 2.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
 	projMatrix2d = glm::ortho(0.0f, (float)WINDOW_WIDTH, (float)WINDOW_HEIGHT, 0.0f, 0.1f, 100.0f);
 	worldMatrix2d = glm::translate(mat4(1.0f), vec3(0.0f, -10.0f, -10.0f));
 }
+
 void Update3D()
 {
 	projMatrix3d = glm::perspective(45.0f, (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
 	viewMatrix = glm::lookAt(vec3(0.0f, 0.0f, 5.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
 	worldMatrix3d = glm::translate(mat4(1.0f), vec3(0.0f, 0.0f, 0.0f));
 }
+
 //Function to update game state
 void update()
 {
-	//projMatrix = glm::perspective(45.0f, (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
 	Update2D();
 	Update3D();
 }
+
 //Main Method - Entry Point
 int main(int argc, char * arg[])
 {
@@ -504,17 +467,14 @@ int main(int argc, char * arg[])
 	{
 		std::cout << "ERROR TFF_Init: " << TTF_GetError();
 	}
+
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false);
+	
 	//Call our InitOpenGL Function
 	initOpenGL();
-	//Call our initGeometry Function
-	//initGeometry();
-	//Set our viewport
+
 	setViewport(WINDOW_WIDTH, WINDOW_HEIGHT);
-	//createShader();
-	//createTexture();
-	//createFontTexture();
-	//initGeometryFromTexture(fontTexture);
+
 	create2DScene();
 	create3DScene();
 	SDL_Event event;
