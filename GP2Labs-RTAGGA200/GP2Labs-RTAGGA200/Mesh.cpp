c42d5d5ab734606ded67b116ffa52f734e2e5759
RossTaggart <rosstaggart12@gmail.com>
2014-10-28 15:29:42 +0000

    Mesh files added

--- GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/Mesh.cpp
+++ GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/Mesh.cpp
@@ -1,30 +1,55 @@
 #include "Mesh.h"
 #include "Component.h"
+#include "Vertex.h"
 
 #include <gl\glew.h>
+//maths headers
+#include <glm/glm.hpp>
+using glm::mat4;
+using glm::vec3;
+using glm::vec2;
 
 void Mesh::init()
 {
+	glGenVertexArrays(1, &m_VAO);
+	glGenVertexArrays(1, &m_VBO);
+	glGenVertexArrays(1, &m_EBO);
 
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)sizeof(vec3));
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)(sizeof(vec3) + sizeof(vec2)));
 }
 
 void Mesh::destroy()
 {
+	glDeleteVertexArrays(1, &m_VAO);
+	glDeleteBuffers(1, &m_EBO);
+	glDeleteBuffers(1, &m_VBO);
 
 }
 
 void Mesh::copyIndexData(int count, int stride, void** data)
 {
-
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, count*stride, data, GL_STATIC_DRAW);
+	m_IndexCount = count;
 }
 
 void Mesh::copyVertexData(int count, int stride, void** data)
 {
-
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+	glBufferData(GL_ARRAY_BUFFER, count*stride, data, GL_STATIC_DRAW);
+	m_VertexCount = count;
 }
 
 void Mesh::Bind()
 {
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+	glBindVertexArray(m_VAO);
 
 }
 
