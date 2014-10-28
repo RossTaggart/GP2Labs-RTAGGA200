ffbfca41cab3326a052401224591891f34e463aa
RossTaggart <rosstaggart12@gmail.com>
2014-10-28 14:46:04 +0000

    created classes
    Mesh.cpp/.h

---
+++ GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/Mesh.h
@@ -0,0 +1,27 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "Component.h"
+
+#include <gl\glew.h>
+
+class Mesh
+{
+	public:
+		void init();
+		void destroy()
+		void copyVertexData(int count, int stride, void** data);
+		void copyIndexData(int count, int stride, void** data);
+		void Bind();
+		GLuint getIndex();
+		GLuint getVertex();
+	private:
+		GLuint m_VBO;
+		GLuint m_EBO;
+		GLuint m_VAO;
+		GLuint m_VertexCount;
+		GLuint m_IndexCount;
+	protected:
+};
+
+#endif
