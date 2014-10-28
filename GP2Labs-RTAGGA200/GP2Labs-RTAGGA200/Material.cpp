4e1da7c88aa324e8acaf90f2a0ba6c4cf850f209
RossTaggart <rosstaggart12@gmail.com>
2014-10-28 15:53:27 +0000

    added classes - ERRORS
    Material.cpp/.h

---
+++ GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/Material.cpp
@@ -0,0 +1,53 @@
+#include "Material.h"
+#include "Component.h"
+
+#include <gl\glew.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <gl\GLU.h>
+#include <SDL_ttf.h>
+#include <glm/glm.hpp>
+
+using namespace std;
+
+using glm::mat4;
+
+void Material::destroy()
+{
+	glDeleteProgram(m_ShaderProgram);
+}
+
+bool Material::loadShader(const string& vertexShader, const string& fragmentShader)
+{
+	GLuint vertexShaderProgram = 0;
+	std::string vsPath = vertexShader;
+	vertexShaderProgram = loadShaderFromFile(vsPath, VERTEX_SHADER);
+
+	GLuint fragmentShaderProgram = 0;
+	std::string fsPath = fragmentShader;
+	fragmentShaderProgram = loadShaderFromFile(fsPath, FRAGMENT_SHADER);
+
+	m_ShaderProgram = glCreateProgram();
+
+	glAttachShader(m_ShaderProgram, vertexShaderProgram);
+	glAttachShader(m_ShaderProgram, fragmentShaderProgram);
+	glLinkProgram(m_ShaderProgram);
+
+	//now we can delete the VS & FS Programs
+	glDeleteShader(vertexShaderProgram);
+	glDeleteShader(fragmentShaderProgram);
+	glBindAttribLocation(m_ShaderProgram, 0, "vertexPosition");
+	glBindAttribLocation(m_ShaderProgram, 1, "vertexTexCoords");
+	glBindAttribLocation(m_ShaderProgram, 2, "vertexColor");
+}
+
+void Material::Bind()
+{
+	glUseProgram(m_ShaderProgram);
+}
+
+GLint Material::getUniformLocation(const string& name)
+{
+	GLint MVPLocation = glGetUniformLocation(m_ShaderProgram, "MVP");
+
+}
---
+++ GP2Labs-RTAGGA200/GP2Labs-RTAGGA200/Material.h
@@ -0,0 +1,24 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include <string>
+
+#include "Component.h"
+#include <gl\glew.h>
+
+
+using namespace std;
+
+class Material
+{
+	public:
+		void destroy();
+		bool loadShader(const string& vertexShader, const string& fragmentShader);
+		void Bind();
+		GLint getUniformLocation(const string& name);
+	private:
+		GLuint m_ShaderProgram;
+	protected:
+};
+
+#endif
